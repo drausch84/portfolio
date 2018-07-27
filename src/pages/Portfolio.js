3d6c997ad26dcd2763ca69bc3fcbb7eeab01eab1
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-07-27 03:16:28 -0600

    finished portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,6 +4,13 @@ import Footer from "../components/Footer";
 import {Col, Row} from "react-materialize";
 import Click from "./images/click.png";
 import "./Portfolio.css";
+import Bamazon from "./images/bamazon.jpg";
+import ClickyGame from "./images/clicky-game.png";
+import FriendFinder from "./images/friendfinder.png";
+import Giphy from "./images/giphyproject.png";
+import Hamburger from "./images/hamburger.png";
+import Liri from "./images/liri.png";
+import TriviaGame from "./images/triviagame.png";
 
 class Portfolio extends Component{
     render(){
@@ -15,7 +22,7 @@ class Portfolio extends Component{
                     <h1 className = "portfolio-header">Portfolio Projects</h1>
                 </Col>
             </Row>
-            <hr width = "75%" />
+            
             <Row>
                 <Col l = {12}>
                 <p className = "portfolio-p">These are a few projects(most done during my time in the University of Denver Coding Boot Camp) to showcase my 
@@ -28,10 +35,12 @@ class Portfolio extends Component{
             <br />
             <Row>
                 <Col l = {6}>
-                    <div className = "image-1"></div>
+                    <div className = "image">
+                        <img src = {ClickyGame}/>
+                    </div>
                 </Col>
                 <Col l = {6}>
-                    <div className = "project-info">
+                    <div className = "project-info-right">
                         <h4>South Park Memory Game</h4>
                         <hr width = "75%"/>
                         <h6>Made with React and MaterializeCSS</h6>
@@ -39,85 +48,124 @@ class Portfolio extends Component{
                     </div>
                 </Col>
             </Row>
-            
+           
             <Row>
                 <Col l = {6}>
-                    <h4>FriendFinder App</h4>
-                    <hr width = "75%"/>
-                    <h6>Made with Node.js, JS, Express.js, MaterializeCSS, and jQuery</h6>
-                    <p className = "link">View <a href = "https://lit-temple-87580.herokuapp.com" target = "_blank"></a></p>   
+                    <div className = "project-info-left">
+                        <h4>FriendFinder App</h4>
+                        <hr width = "75%"/>
+                        <h6>Made with Node.js, JS, Express.js, MaterializeCSS, and jQuery</h6>
+                        <p className = "link">View <a href = "https://lit-temple-87580.herokuapp.com" target = "_blank"><img src = {Click} className = "click-btn"/></a></p>   
+                    </div>
                 </Col>
                 <Col l ={6}>
-                    <div className = "image-2"></div>      
+                    <div className = "image">
+                        <img src = {FriendFinder}/>
+                    </div>      
                 </Col>
             </Row>
 
             <Row>
                 <Col l = {6}>
-                    <div className = "image-3"></div>
+                    <div className = "image">
+                        <img src = {Bamazon}/>
+                    </div>
                 </Col>
                 <Col l = {6}>
-                    <h4>Bamazon</h4>
-                    <hr width = "75%"/>
-                    <h6>Made with Node.js and MySQL</h6>
-                    <p className = "link">View<a href = "https://www.youtube.com/watch?v=VP7JfELPBxc&t=4s" target = "_blank"></a></p>
-                </Col>
-            </Row>
-
-            <Row>
-                <Col l = {6}>
-                    <h4>Giphy Gif Project</h4>
-                    <hr width = "75%"/>
-                    <h6>Made with API integration, JS, and BootstrapCSS</h6>   
-                    <p className = "link">View<a href = "https://drausch84.github.io/Giphy-Project/" target = "_blank"></a></p>  
-                </Col>
-                <Col l = {6}>
-                    <div className = "image-4"></div>
+                    <div className = "project-info-right">
+                        <h4>Bamazon</h4>
+                        <hr width = "75%"/>
+                        <h6>Made with Node.js and MySQL</h6>
+                        <p className = "link">View<a href = "https://www.youtube.com/watch?v=VP7JfELPBxc&t=4s" target = "_blank"><img src = {Click} className = "click-btn"/></a></p>
+                    </div>
                 </Col>
             </Row>
 
             <Row>
                 <Col l = {6}>
-                    <div className = "image-5"></div>
+                    <div className = "project-info-left">
+                        <h4>Giphy Gif Project</h4>
+                        <hr width = "75%"/>
+                        <h6>Made with API integration, JS, and BootstrapCSS</h6>   
+                        <p className = "link">View<a href = "https://drausch84.github.io/Giphy-Project/" target = "_blank"><img src = {Click} className = "click-btn"/></a></p>  
+                    </div>
                 </Col>
                 <Col l = {6}>
-                    <h4>Eat Da Burger App</h4>
-                    <hr width = "75%"/>
-                    <h6>Made with Node.js, Express.js, MySQL, Handlebars.js, JS, and MaterializeCSS</h6>   
-                    <p className = "link">View<a href = "https://afternoon-refuge-63972.herokuapp.com" target = "_blank"></a></p>       
-                </Col>
-            </Row>
-
-            <Row>
-                <Col l = {6}>
-                    <h4>LIRI App</h4>
-                    <hr width = "75%"/>
-                    <h6>Made with API integration and Node.js</h6>
-                    <p className = "link">View<a href = "https://www.youtube.com/watch?v=3H4b1NIrjt4&t=3s" target = "_blank"></a></p>   
-                    
-                </Col>
-                <Col l = {6}>
-                    <div className = "image-6"></div>
+                    <div className = "image">
+                        <img src = {Giphy}/>
+                    </div>
                 </Col>
             </Row>
 
             <Row>
                 <Col l = {6}>
-                    <div className= "image-7"></div>
+                    <div className = "image">
+                        <img src = {Hamburger}/>
+                    </div>
                 </Col>
                 <Col l = {6}>
-                    <h4>Breaking Bad Trivia Game</h4>
-                    <hr width = "75%"/>
-                    <h6>Made with JS and jQuery</h6>    
-                    <p className = "link">View<a href = "https://drausch84.github.io/TriviaGame/" target = "_blank"></a></p>                           
+                    <div className = "project-info-right">
+                        <h4>Eat Da Burger App</h4>
+                        <hr width = "75%"/>
+                        <h6>Made with Node.js, Express.js, MySQL, Handlebars.js, JS, and MaterializeCSS</h6>   
+                        <p className = "link">View<a href = "https://afternoon-refuge-63972.herokuapp.com" target = "_blank"><img src = {Click} className = "click-btn"/></a></p>       
+                    </div>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col l = {6}>
+                    <div className = "project-info-left">
+                        <h4>LIRI App</h4>
+                        <hr width = "75%"/>
+                        <h6>Made with API integration and Node.js</h6>
+                        <p className = "link">View<a href = "https://www.youtube.com/watch?v=3H4b1NIrjt4&t=3s" target = "_blank"><img src = {Click} className = "click-btn"/></a></p>   
+                    </div>
+                </Col>
+                <Col l = {6}>
+                    <div className = "image">
+                        <img src = {Liri}/>
+                    </div>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col l = {6}>
+                    <div className= "image">
+                        <img src = {TriviaGame}/>
+                    </div>
+                </Col>
+                <Col l = {6}>
+                    <div className = "project-info-right">
+                        <h4>Breaking Bad Trivia Game</h4>
+                        <hr width = "75%"/>
+                        <h6>Made with JS and jQuery</h6>    
+                        <p className = "link">View<a href = "https://drausch84.github.io/TriviaGame/" target = "_blank"><img src = {Click} className = "click-btn"/></a></p>                           
+                    </div>
                 </Col>
             </Row>
             <br />
-            <Row></Row>
-            <Row></Row>
+            <Row>
+
+            </Row>
+            <Row>
+
+            </Row>
             <br />
-            <Row></Row>
-            <Row></Row>
+            <Row>
+
+            </Row>
+            <Row>
+
+            </Row>
+            <Row>
+            </Row>
+            <Row>
+            </Row>
+            <Row>
+            </Row>
+            <Row>
+            </Row>
                 <Footer />
             </div>    
         );
