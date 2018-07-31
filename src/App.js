af7d62775ba03a874352f36f33d085f0c6d894d0
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-07-30 21:54:37 -0600

    added 404 page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, {Component} from 'react';
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
+// import {CSSTransition, TransitionGroup, Switch} from "react-transition-group";
+// import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Skills from "./pages/Skills";
 import Art from "./pages/Art";
 import Contact from "./pages/Contact";
+import PageNotFound from "./pages/404";
 
 class App extends Component{
   render(){
     return(
       <div className = "App">
+        <Switch>
         <Route path = "/" exact component = {Home}></Route>
         <Route path = "/about-me" exact component = {About}></Route>
         <Route path = "/portfolio" exact component = {Portfolio}></Route>
         <Route path = "/skills" exact component = {Skills}></Route>
         <Route path = "/art" exact component = {Art}></Route>
         <Route path = "/contact" exact component = {Contact}></Route>
-      </div>  
+        <Route path = "*" exact component = {PageNotFound}></Route>
+        </Switch>
+      </div> 
+      
+
     )
   }
 }
---
+++ src/pages/404.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import {Col, Row} from "react-materialize";
+import "./404.css";
+import ErrorImg from "./images/404-image.jpg";
+
+class PageNotFound extends Component{
+    render(){
+        return (
+            <div>
+               
+                        <img src = {ErrorImg} alt = "error-img" className = "error-img" />
+                
+                <Row>
+                    <Col l = {12}>
+                        <h3 className = "redirect">Or you can just click <a href = "/">Here</a> and you'll be redirected to the home page</h3>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
+
+export default PageNotFound;
