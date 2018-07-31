4834e0b88667310aee82f9549bf749428fe0af2c
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-07-31 12:58:30 -0600

    animated page transitions added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import {Route, Switch} from "react-router-dom";
-// import {CSSTransition, TransitionGroup, Switch} from "react-transition-group";
-// import "./App.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
+import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -9,21 +9,35 @@ import Skills from "./pages/Skills";
 import Art from "./pages/Art";
 import Contact from "./pages/Contact";
 import PageNotFound from "./pages/404";
+import PageShell from "./components/PageShell";
 
 class App extends Component{
   render(){
     return(
+      
+      <Route render = {({location}) => (
       <div className = "App">
-        <Switch>
-        <Route path = "/" exact component = {Home}></Route>
-        <Route path = "/about-me" exact component = {About}></Route>
-        <Route path = "/portfolio" exact component = {Portfolio}></Route>
-        <Route path = "/skills" exact component = {Skills}></Route>
-        <Route path = "/art" exact component = {Art}></Route>
-        <Route path = "/contact" exact component = {Contact}></Route>
-        <Route path = "*" exact component = {PageNotFound}></Route>
-        </Switch>
-      </div> 
+        
+        <TransitionGroup>
+          <CSSTransition
+            key = {location.key}
+            timeout={300}
+            classNames = "fade"
+            >
+            <Switch location = {location}>
+              <Route path = "/" exact component = {PageShell(Home)}></Route>
+              <Route path = "/about-me" exact component = {PageShell(About)}></Route>
+              <Route path = "/portfolio" exact component = {PageShell(Portfolio)}></Route>
+              <Route path = "/skills" exact component = {PageShell(Skills)}></Route>
+              <Route path = "/art" exact component = {PageShell(Art)}></Route>
+              <Route path = "/contact" exact component = {PageShell(Contact)}></Route>
+              <Route path = "*" exact component = {PageShell(PageNotFound)}></Route>
+            </Switch>
+          </CSSTransition>
+      </TransitionGroup>
+    
+      // </div> 
+        )}/>
       
 
     )
---
+++ src/components/PageShell.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+const PageShell = Page => {
+    return props =>
+        <div className = "page">
+            <ReactCSSTransitionGroup
+        
+        transitionAppear={true}
+        transitionAppearTimeout={600}
+        transitionEnterTimeout={600}
+        transitionLeaveTimeout={200}
+        transitionName={props.match.path === "/" || "/about-me" || "/portfolio" || "/skills" || "/art" || "/contact" || "*" ? 'SlideIn' : 'SlideOut' }
+      >
+       
+        <Page {...props} />
+      </ReactCSSTransitionGroup>
+        </div>
+
+
+}
+
+export default PageShell;
