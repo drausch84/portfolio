685620bc6eec029165de8f49c8b8a198443a32f8
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-07-31 19:03:39 -0600

    home page ui changes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Hero.css";
-import Picture from "./me.jpg";
+import Picture from "./me-beach.jpg";
 
 function Hero (){
     return <img src = {Picture} className = "profile-pic"/>
